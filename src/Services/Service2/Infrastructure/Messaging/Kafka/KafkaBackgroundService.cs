0d46d1d16a6a5a4247a495547477801765a50228
akhmetov_rr <robertakhmetov@bk.ru>
2026-05-24 11:22:30 +0300

    KafkaBackgroundService template created

---
+++ src/Services/Service2/Infrastructure/Messaging/Kafka/KafkaBackgroundService.cs
@@ -0,0 +1,9 @@
+using Microsoft.Extensions.Hosting;
+
+public class KafkaBackgroundService : BackgroundService
+{
+    protected override Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        throw new NotImplementedException();
+    }
+}
